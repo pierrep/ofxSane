2bbfc0af849f0d55de05c351b33778326dfcb155
Pierre Proske <pierre@sensoryempire.com>
2018-02-07 13:48:04 +1100

    code cleanup

--- src/ofxSane.cpp
+++ src/ofxSane.cpp
@@ -1,15 +1,15 @@
 #include "ofxSane.h"
 
 void printStatus(SANE_Status status) {
-    cout << "status: " << sane_strstatus(status) << endl;
+    ofLogVerbose() << "status: " << sane_strstatus(status);
 }
 
 void printDevice(const SANE_Device* device) {
-    cout << "device:" << endl <<
-        "\tname = " << device->name << endl <<
-        "\tvendor = " << device->vendor << endl <<
-        "\tmodel = " << device->model << endl <<
-        "\ttype = " << device->type << endl;
+    ofLogVerbose() << "device:" <<
+        "\tname = " << device->name <<
+        "\tvendor = " << device->vendor <<
+        "\tmodel = " << device->model <<
+        "\ttype = " << device->type;
 }
 
 string typeToString(const SANE_Value_Type& type) {
@@ -38,26 +38,190 @@ string unitToString(const SANE_Unit& unit) {
 }
 
 void printDescriptor(const SANE_Option_Descriptor* descriptor) {
-    cout << descriptor->title << " " <<
+    ofLogVerbose() << descriptor->title << " " <<
         "(" << typeToString(descriptor->type) <<
         "/" << unitToString(descriptor->unit) << ")" <<
         "[" << descriptor->size << "] ";
     switch(descriptor->constraint_type) {
         case SANE_CONSTRAINT_NONE:
-            cout << "any value";
+            ofLogVerbose() << "any value";
             break;
         case SANE_CONSTRAINT_STRING_LIST:
-            cout << "{string list...}";
+            ofLogVerbose() << "{string list...}";
             break;
         case SANE_CONSTRAINT_WORD_LIST:
-            cout << "{word list...}";
+            ofLogVerbose() << "{word list...}";
             break;
         case SANE_CONSTRAINT_RANGE:
             const SANE_Range* range = descriptor->constraint.range;
-            cout << range->min << " to " << range->max;
+            ofLogVerbose() << range->min << " to " << range->max;
             if(range->quant != 0)
-                cout << " in " << range->quant;
+                ofLogVerbose() << " in " << range->quant;
             break;
     }
-    cout << endl;
+    ofLogVerbose();
+}
+
+//--------------------------------------------------------------------
+ofxSane::ofxSane()
+{
+    totalDevices = 0;
+    bufferSize = 0;
+    buffer = NULL;
+    scanning = false;
+    pixelsPerLine = 0;
+    totalLines = 0;
+}
+
+//--------------------------------------------------------------------
+ofxSane::~ofxSane() {
+    if(totalDevices > 0) {
+        ofLogNotice() << "closing SANE";
+        sane_cancel(handle);
+        sane_close(handle);
+    }
+
+    sane_exit();
+
+    if(buffer != NULL)
+        delete [] buffer;
+}
+
+//--------------------------------------------------------------------
+void ofxSane::setup() {
+    SANE_Int versionCode;
+
+    ofLogNotice() << "Initializing SANE: ";
+    printStatus(sane_init(&versionCode, NULL));
+    ofLogNotice() << "running SANE v" << SANE_VERSION_MAJOR(versionCode) << "." << SANE_VERSION_MINOR(versionCode);
+
+    ofLogVerbose() << "sane_get_devices: ";
+    const SANE_Device** deviceList;
+    printStatus(sane_get_devices(&deviceList, true));
+
+    int curDevice;
+    for(curDevice = 0; deviceList[curDevice] != NULL; curDevice++) {
+        printDevice(deviceList[curDevice]);
+    }
+    totalDevices = curDevice;
+    ofLogNotice() << "Found " << totalDevices << " devices total";
+
+    if(totalDevices > 0) {
+        SANE_String_Const name = deviceList[0]->name;
+        ofLogVerbose() << "sane_open(" << name << "): ";
+        printStatus(sane_open(name, &handle));
+
+        // use sane_get_option_descriptor to get options
+        int totalDescriptors;
+        sane_control_option (handle, 0, SANE_ACTION_GET_VALUE, &totalDescriptors, 0);
+        ofLogVerbose() << totalDescriptors << " descriptors total:";
+        for(int i = 0; i < totalDescriptors; i++) {
+            const SANE_Option_Descriptor* cur = sane_get_option_descriptor(handle, i);
+            ofLogVerbose() << i << ": ";
+            printDescriptor(cur);
+        }
+
+        SANE_Char mode[] = "Color";
+        ofLogVerbose() << "setting mode to " << mode << ": ";
+        printStatus(sane_control_option(handle, 2, SANE_ACTION_SET_VALUE, &mode, NULL));
+
+        SANE_Int dpi = 300;
+        ofLogVerbose() << "setting dpi to " << dpi << ": ";
+        printStatus(sane_control_option(handle, 6, SANE_ACTION_SET_VALUE, &dpi, NULL));
+
+        SANE_Int bitdepth = 8;
+        ofLogVerbose() << "setting bitdepth to " << bitdepth << ": ";
+        printStatus(sane_control_option(handle, 5, SANE_ACTION_SET_VALUE, &bitdepth, NULL));
+
+        SANE_Int xsize = 215;
+        ofLogVerbose() << "setting xsize to " << xsize << " mm" << ": ";
+        xsize *= (1 << 16);
+        printStatus(sane_control_option(handle, 10, SANE_ACTION_SET_VALUE, &xsize, NULL));
+    }
+}
+
+//--------------------------------------------------------------------
+void ofxSane::scan() {
+    if(isThreadRunning())
+        stopThread();
+    if(totalDevices > 0)
+        startThread(false);
+}
+
+//--------------------------------------------------------------------
+void ofxSane::threadedFunction()
+{
+    scanning = true;
+
+    ofLogNotice() << "Starting scan...";
+    printStatus(sane_start(handle));
+
+    SANE_Parameters parameters;
+    printStatus(sane_get_parameters(handle, &parameters));
+
+    ofLogVerbose() << "depth: " << parameters.depth;
+
+    totalLines = parameters.lines;
+    ofLogVerbose() << "lines: " << parameters.lines;
+
+    pixelsPerLine = parameters.pixels_per_line;
+    ofLogVerbose() << "pixels per line: " << parameters.pixels_per_line;
+
+    bufferSize = parameters.bytes_per_line;
+    SANE_Int size;
+
+    ofLogVerbose() << "bufferSize (bytes per line) = " << bufferSize;
+
+    if(buffer == NULL)
+        buffer = new unsigned char[bufferSize];
+
+    SANE_Status status = SANE_STATUS_GOOD;
+
+    float start = ofGetElapsedTimef();
+    float lines = 0;
+    while(scanning && status == SANE_STATUS_GOOD) {
+        status = sane_read(handle, buffer, bufferSize, &size);
+        lineEventArgs eventArgs;
+        eventArgs.line = buffer;
+        eventArgs.size = size;
+        ofNotifyEvent(lineEvent, eventArgs);
+        lines++;
+    }
+    float stop = ofGetElapsedTimef();
+    ofLogVerbose() << start << " to " << stop << " with " << lines << " lines";
+    float scanLineRate = (lines / (stop - start));
+    ofLogVerbose() <<  scanLineRate << " lps";
+
+    ofLogVerbose() << "final status is: ";
+    printStatus(status);
+
+    sane_cancel(handle);
+    scanning = false;
+}
+
+//--------------------------------------------------------------------
+void ofxSane::stop() {
+    scanning = false;
+}
+
+//--------------------------------------------------------------------
+int ofxSane::getBufferSize() {
+    return bufferSize;
+}
+
+//--------------------------------------------------------------------
+const unsigned char* ofxSane::getBuffer() {
+    return buffer;
+}
+
+//--------------------------------------------------------------------
+size_t ofxSane::getPixelsPerLine()
+{
+    return static_cast<size_t>(pixelsPerLine);
+}
+
+//--------------------------------------------------------------------
+size_t ofxSane::getTotalLines()
+{
+    return static_cast<size_t>(totalLines);
 }
--- src/ofxSane.h
+++ src/ofxSane.h
@@ -11,11 +11,28 @@ void printDescriptor(const SANE_Option_Descriptor* descriptor);
 
 class lineEventArgs : public ofEventArgs {
 public:
-    unsigned char* line;
     int size;
+    unsigned char* line;
 };
 
 class ofxSane : public ofThread {
+
+public:
+    ofxSane();
+    ~ofxSane();
+    void setup();
+    void scan();
+    void stop();
+    int getBufferSize();
+    const unsigned char* getBuffer();
+    size_t getPixelsPerLine();
+    size_t getTotalLines();
+
+    ofEvent<lineEventArgs> lineEvent;    
+
+protected:
+    void threadedFunction();
+
 private:
     SANE_Handle handle;
     int totalDevices;
@@ -24,147 +41,4 @@ private:
     int totalLines;
     bool scanning;
     unsigned char* buffer;
-public:
-    ofxSane() {
-        totalDevices = 0;
-        bufferSize = 0;
-        buffer = NULL;
-        scanning = false;
-        pixelsPerLine = 0;
-        totalLines = 0;
-    }
-    ~ofxSane() {
-        if(totalDevices > 0) {
-            cout << "sane_close" << endl;
-            sane_cancel(handle);
-            sane_close(handle);
-        }
-        sane_exit();
-        if(buffer != NULL)
-            delete [] buffer;
-    }
-    void setup() {
-        SANE_Int versionCode;
-        cout << "sane_init: ";
-        printStatus(sane_init(&versionCode, NULL));
-        cout << "running SANE v" << SANE_VERSION_MAJOR(versionCode) << "." << SANE_VERSION_MINOR(versionCode) << endl;
-
-        cout << "sane_get_devices: ";
-        const SANE_Device** deviceList;
-        printStatus(sane_get_devices(&deviceList, true));
-
-        int curDevice;
-        for(curDevice = 0; deviceList[curDevice] != NULL; curDevice++)
-            printDevice(deviceList[curDevice]);
-        totalDevices = curDevice;
-        cout << totalDevices << " devices total" << endl;
-
-        if(totalDevices > 0) {
-            SANE_String_Const name = deviceList[0]->name;
-            cout << "sane_open(" << name << "): ";
-            printStatus(sane_open(name, &handle));
-
-            // use sane_get_option_descriptor to get options
-            int totalDescriptors;
-            sane_control_option (handle, 0, SANE_ACTION_GET_VALUE, &totalDescriptors, 0);
-            cout << totalDescriptors << " descriptors total:" << endl;
-            for(int i = 0; i < totalDescriptors; i++) {
-                const SANE_Option_Descriptor* cur = sane_get_option_descriptor(handle, i);
-                cout << i << ": ";
-                printDescriptor(cur);
-            }
-
-            SANE_Char mode[] = "Color";
-            cout << "setting mode to " << mode << ": ";
-            printStatus(sane_control_option(handle, 2, SANE_ACTION_SET_VALUE, &mode, NULL));
-
-            SANE_Int dpi = 300;
-            cout << "setting dpi to " << dpi << ": ";
-            printStatus(sane_control_option(handle, 6, SANE_ACTION_SET_VALUE, &dpi, NULL));
-
-            SANE_Int bitdepth = 8;
-            cout << "setting bitdepth to " << bitdepth << ": ";
-            printStatus(sane_control_option(handle, 5, SANE_ACTION_SET_VALUE, &bitdepth, NULL));
-
-            SANE_Int xsize = 215;
-            cout << "setting xsize to " << xsize << " mm" << ": ";
-            xsize *= (1 << 16);
-            printStatus(sane_control_option(handle, 10, SANE_ACTION_SET_VALUE, &xsize, NULL));
-        }
-    }
-    void scan() {
-        if(isThreadRunning())
-            stopThread();
-        if(totalDevices > 0)
-            startThread(false, false);
-    }
-    void stop() {
-        scanning = false;
-    }
-    ofEvent<lineEventArgs> lineEvent;
-    int getBufferSize() {
-        return bufferSize;
-    }
-    const unsigned char* getBuffer() {
-        return buffer;
-    }
-    int getPixelsPerLine()
-    {
-        return pixelsPerLine;
-    }
-    int getTotalLines()
-    {
-        return totalLines;
-    }
-
-protected:
-    void threadedFunction() {
-        scanning = true;
-
-        cout << "sane_start" << endl;
-        printStatus(sane_start(handle));
-
-        SANE_Parameters parameters;
-        cout << "sane_get_parameters" << endl;
-        printStatus(sane_get_parameters(handle, &parameters));
-
-        cout << "depth: " << parameters.depth << endl;
-
-        totalLines = parameters.lines;
-        cout << "lines: " << parameters.lines << endl;
-
-        pixelsPerLine = parameters.pixels_per_line;
-        cout << "pixels per line: " << parameters.pixels_per_line << endl;
-
-        bufferSize = parameters.bytes_per_line;
-        SANE_Int size;
-
-        cout << "bufferSize (bytes per line) = " << bufferSize << endl;
-
-        if(buffer == NULL)
-            buffer = new unsigned char[bufferSize];
-
-        SANE_Status status = SANE_STATUS_GOOD;
-        cout << "while(status == SANE_STATUS_GOOD)..." << endl;
-        float start = ofGetElapsedTimef();
-        float lines = 0;
-        while(scanning && status == SANE_STATUS_GOOD) {
-            status = sane_read(handle, buffer, bufferSize, &size);
-            lineEventArgs eventArgs;
-            eventArgs.line = buffer;
-            eventArgs.size = size;
-            ofNotifyEvent(lineEvent, eventArgs);
-            lines++;
-        }
-        float stop = ofGetElapsedTimef();
-        cout << start << " to " << stop << " with " << lines << " lines" << endl;
-        float scanLineRate = (lines / (stop - start));
-        cout <<  scanLineRate << " lps" << endl;
-
-        cout << "final status is: ";
-        printStatus(status);
-
-        cout << "sane_cancel" << endl;
-        sane_cancel(handle);
-    }
 };
